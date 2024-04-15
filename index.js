ea3251461c9ece8edf42f9554b2061d041df711f
Pedro-MB-Cruz <pedrombcruz@outlook.com>
2024-04-15 11:54:43 +0100

    teste

---
+++ index.js
@@ -0,0 +1,18 @@
+require('dotenv').config();
+
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const express = require('express');
+
+//const router = require('./router');
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors())
+
+//app.use('/api', router);
+
+const port = process.env.PORT || 8081;
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
